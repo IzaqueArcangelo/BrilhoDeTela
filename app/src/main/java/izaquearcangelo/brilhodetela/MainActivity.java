ab279f44cf5b8f75f674439569c7ea6866f656d0
Izaque <izk_arcangelo@hotmail.com>
2015-09-16 19:25:16 -0300

    Atualização nos comentários para compreender melhor o código.

--- app/src/main/java/izaquearcangelo/brilhodetela/MainActivity.java
+++ app/src/main/java/izaquearcangelo/brilhodetela/MainActivity.java
@@ -13,7 +13,9 @@ import android.view.WindowManager;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-/** Classe principal da aplicação */
+/**
+ * Classe principal da aplicação
+ */
 public class MainActivity extends Activity {
 
     private SeekBar brightbar;
@@ -26,75 +28,68 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Instantiate seekbar object
+
+        // Instancia do objeto seekbar utilizado na tela.
         brightbar = (SeekBar) findViewById(R.id.brightbar);
 
+        // Texto que exibe a mensagem e a porcentagem na tela.
         txtPerc = (TextView) findViewById(R.id.txtPercentage);
 
-        //Get the content resolver
+        // "Provedor de conteúdo" -  obtido do contexto da aplicação
         cResolver = getContentResolver();
 
-        //Get the current window
+        // Pega a janela atual.
         window = getWindow();
 
         //Define o intervalo do seekbar entre  0 e 255
         brightbar.setMax(255);
+
         //Define o incremento da barra
         brightbar.setKeyProgressIncrement(1);
 
-        try
-        {
+        try {
             //Obtem o brilho do Sistema atual.
             brightness = Settings.System.getInt(cResolver, Settings.System.SCREEN_BRIGHTNESS);
-        }
-        catch (Settings.SettingNotFoundException e)
-        {
-            //Throw an error case it couldn't be retrieved
-            Log.e("Error", "Cannot access system brightness");
+        } catch (Settings.SettingNotFoundException e) {
+            //Lança uma exceção se acontecer um erro genérico.
+            Log.e("Error", "Não foi possível obter o brilho do Sistema.");
             e.printStackTrace();
         }
 
-        //Set the progress of the seek bar based on the system's brightness
+        // Define a luminosidade do sistema de acordo com o progresso da barra.
         brightbar.setProgress(brightness);
 
         //Register OnSeekBarChangeListener, so it can actually change values
-        brightbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
-        {
-            public void onStopTrackingTouch(SeekBar seekBar)
-            {
+        brightbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onStopTrackingTouch(SeekBar seekBar) {
                 //Set the system brightness using the brightness variable value
-
                 Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
                 //Get the current window attributes
                 WindowManager.LayoutParams layoutpars = window.getAttributes();
                 //Set the brightness of this window
-                layoutpars.screenBrightness = brightness / (float)255;
+                layoutpars.screenBrightness = brightness / (float) 255;
                 //Apply attribute changes to this window
                 window.setAttributes(layoutpars);
             }
 
-            public void onStartTrackingTouch(SeekBar seekBar)
-            {
+            public void onStartTrackingTouch(SeekBar seekBar) {
                 //Nothing handled here
             }
 
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-            {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 //Define o mínimo de brilho para o seekbar
-                if(progress<=1)
-                {
+                if (progress <= 1) {
                     //Set the brightness to 20
-                    brightness=1;
-                }
-                else //brightness is greater than 20
+                    brightness = 1;
+                } else //brightness is greater than 20
                 {
                     //Set brightness variable based on the progress bar
                     brightness = progress;
                 }
                 //Calculate the brightness percentage
-                float perc = (brightness /(float)255)*100;
+                float perc = (brightness / (float) 255) * 100;
                 //Set the brightness percentage
-                txtPerc.setText((int)perc +" %");
+                txtPerc.setText((int) perc + " %");
             }
         });
     }
